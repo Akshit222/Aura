fe93f794504ba3bca80f4ddc4c6af127cfc161c0
Akshit Bhatt <akshitbhattab@gmail.com>
2025-02-09 02:52:33 +0530

    Update index.js

--- DashboardWithAuthandTherapyCards/DashboardWithAuthAndTherapyCards/server/index.js
+++ DashboardWithAuthandTherapyCards/DashboardWithAuthAndTherapyCards/server/index.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = 4002;
 
 // Initialize sentiment analyzer
 const analyzer = new natural.SentimentAnalyzer(
